9e6d73ffb5b23d723ce51500a07ca26da8e3dd23
avinash-kumar-iit <avinashkr.cseiitbhu@gmail.com>
2023-05-20 19:00:37 +0530

    emailsender code

---
+++ src/main/java/com/hdfc/emailSender/controller/EmailSenderController.java
@@ -0,0 +1,37 @@
+package com.hdfc.emailSender.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hdfc.emailSender.model.EmailRequest;
+import com.hdfc.emailSender.service.EmailSenderImpl;
+
+@RestController
+public class EmailSenderController {
+	
+	@Autowired
+	private  EmailSenderImpl emailService;
+
+//    @Autowired
+//    public EmailSenderController(EmailSenderImpl emailService) {
+//        this.emailService = emailService;
+//    }
+
+//    @PostMapping(value="/send-email", consumes = { MediaType.APPLICATION_JSON_VALUE })
+//    public void sendEmail(@RequestBody EmailRequest emailRequest) {
+//        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getBody());
+//    }
+    @PostMapping("/send-email")
+    public String  sendEmail() {
+    	String s="Nirakara.dash03@gmail.com";
+        emailService.sendEmail(s, "creating email by java code", "Hi Nirakar Sir"
+        		+ " I am able to sent email by smtp server by using java sppring boot code "
+        		+ "Thanks Avinash Kumar");
+        return "Mail Sent Success!";
+    }
+
+}
---
+++ src/main/java/com/hdfc/emailSender/model/EmailRequest.java
@@ -0,0 +1,28 @@
+package com.hdfc.emailSender.model;
+
+
+public class EmailRequest {
+	
+	String to;
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	String subject;
+	String body;
+
+}
